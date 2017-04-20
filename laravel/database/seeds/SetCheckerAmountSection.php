5749b38f216e595a862800f69ec99398e0d391df
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-04-20 14:26:35 +0700

    Make seeder

---
+++ laravel/database/seeds/SetCheckerAmountSection.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SetCheckerAmountSection extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            "head" => "2",
+            "recreation" => "2",
+            "camp_app" => "3",
+            "camp_game" => "3",
+            "camp_network" => "3",
+            "camp_iot" => "3",
+            "camp_datasci" => "3",
+        ];
+
+        foreach ($camps as $key => $value) {
+            DB::table('sections')->where('name', $key)->update(['checker_amount' => $value]);
+        }
+    }
+}
